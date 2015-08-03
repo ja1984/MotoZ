5e8b8caf5c3e255198835f9368eb882199afeaa1
Jonathan Andersson <jonathan@ezy.se>
2015-08-03 16:36:09 +0200

    Added all files needed for gettings started

---
+++ App/desktop/src/se/ja1984/motoz/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package se.ja1984.motoz.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import se.ja1984.motoz.GameClass;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GameClass(), config);
+	}
+}
---
+++ App/html/src/se/ja1984/motoz/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package se.ja1984.motoz.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import se.ja1984.motoz.GameClass;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new GameClass();
+        }
+}
